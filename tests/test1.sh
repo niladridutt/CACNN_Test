2642fd6c2ca0c17f2b6f345787c8488298c2787d
Niladri Shekhar Dutt <niladrishekhardutt@gmail.com>
2019-04-27 20:23:58 -0700

    Update test1.sh

--- tests/test1.sh
+++ tests/test1.sh
@@ -10,13 +10,13 @@ __SIGMAW=1
 __SIGMAH=1
 
 __C_B=3
-__K_B=12
-__W_B=3
-__H_B=3
-__RP_B=3
+__K_B=3
+__W_B=11
+__H_B=11
+__RP_B=11
 __RPP_B=1
-__SP_B=3
-__SPP_B=4
+__SP_B=11
+__SPP_B=1
 
 ./measure $__C $__K $__W $__H $__R $__S $__SIGMAW $__SIGMAH $__C_B $__K_B $__W_B $__H_B $__RP_B $__RPP_B $__SP_B $__SPP_B;
 
