ae466645b35fda6f69ae5e86eed4aa4227ce7d93
MaxT <854721132@qq.com>
2019-04-28 18:12:49 -0700

    Uploaded test cases

---
+++ tests/test_mem4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+__C=3
+__K=96
+__W=55
+__H=55
+__R=11
+__S=11
+__SIGMAW=1
+__SIGMAH=1
+
+__C_B=1
+__K_B=1
+__W_B=1
+__H_B=4
+__RP_B=1
+__RPP_B=1
+__SP_B=4
+__SPP_B=1
+
+./measure $__C $__K $__W $__H $__R $__S $__SIGMAW $__SIGMAH $__C_B $__K_B $__W_B $__H_B $__RP_B $__RPP_B $__SP_B $__SPP_B;
